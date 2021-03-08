45af800f02ede527a3c06a817d5cf29fe6083ac6
Sasmitha3001 <65769282+Sasmitha3001@users.noreply.github.com>
2021-03-08 16:16:30 +0530

    Add files via upload

---
+++ Screens/ClassDetails.js
@@ -0,0 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+
+export default class ClassDetails extends Component {
+  render(){
+    return (
+      <View>
+        <Card>
+        <Text>Class Details</Text>
+        </Card>
+      </View>
+    )
+  }
+  
+}
+
